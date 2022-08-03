5e40741d0d4099477acf34166eb2ec2f1409d708
Quele2022 <quele.ap.galdino@gmail.com>
2022-08-03 17:21:36 -0300

    projeto-cookenu

---
+++ modulo5/projeto-cookenu/src/controller/RecipeController.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from "express";
+import { RecipeDatabase } from "../database/RecipeDatabase";
+import { Recipe } from "../models/Recipe";
+import { Authenticator } from "../services/Authenticator";
+import { IdGenerator } from "../services/IdGenerator";
+
+export class RecipeController {
+    public getAllRecipes = async (req: Request, res: Response) => {
+        let errorCode = 400
+        try {
+            const token = req.headers.authorization
+
+            if (!token) {
+                errorCode = 401
+                throw new Error("Token faltando")
+            }
+
+            const authenticator = new Authenticator()
+            const payload = authenticator.getTokenPayload(token)
+
+            if (!payload) {
+                errorCode = 401
+                throw new Error("Token inválido")
+            }
+
+            const recipeDatabase = new RecipeDatabase()
+            const recipesDB = await recipeDatabase.getAllRecipes()
+
+            const recipes = recipesDB.map((recipeDB) => {
+                return new Recipe(
+                    recipeDB.id,
+                    recipeDB.title,
+                    recipeDB.description,
+                    recipeDB.created_at,
+                    recipeDB.updated_at,
+                    recipeDB.creator_id
+                )
+            })
+
+            res.status(200).send({ recipes })
+        } catch (error) {
+            res.status(errorCode).send({ message: error.message })
+        }
+    }
+
+    public createRecipe = async (req: Request, res: Response) => {
+let errorCode = 400
+try {
+    const token = req.headers.authorization
+    const {title, description} = req.body
+if(!title || !description){
+errorCode = 422
+throw new Error("parametro faltando")
+}
+if(title !== "string" || description !== "string"){
+errorCode = 415
+throw new Error("tipo de dado incorreto") 
+}
+const id = new IdGenerator().generate()
+const newrecipe = new Recipe(id, title, description, new Date.now().toString(), new Date.now().toString())
+
+} catch (error) {
+    
+}
+    }
+}
