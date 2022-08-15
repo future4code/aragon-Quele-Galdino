5a479da1eb9ecc19f5fd1f5b3f1705bac496e557
Quele2022 <quele.ap.galdino@gmail.com>
2022-08-15 17:40:28 -0300

    atividade testes-jest

---
+++ modulo5/testes-jest/tests/postbusiness/createPost.test.ts
@@ -0,0 +1,12 @@
+import {PostBusiness} from "../../src/business/PostBusiness"
+import { PostDatabaseMock } from "../mocks/postDatabaseMock"
+import { AuthenticatorMock } from "../mocks/services/AuthenticatorMock"
+import { HashManagerMock } from "../mocks/services/HashManagerMock"
+import { IdGeneratorMock } from "../mocks/services/IdGeneratorMock"
+
+describe("testando PostBusiness", () => {
+    const postBusiness = new PostBusiness(new PostDatabaseMock(),
+     new IdGeneratorMock(),
+      new HashManagerMock(),
+      new AuthenticatorMock())
+})  
