68f83faed36c790f97053bad2599e6bfcf06845c
Quele2022 <quele.ap.galdino@gmail.com>
2022-08-22 16:30:06 -0300

    feat: start project

--- modulo5/testes-jest/tests/postbusiness/createPost.test.ts
+++ modulo5/testes-jest/tests/postbusiness/createPost.test.ts
@@ -1,4 +1,5 @@
 import {PostBusiness} from "../../src/business/PostBusiness"
+import { ICreatePostInputDTO } from "../../src/models/Post"
 import { PostDatabaseMock } from "../mocks/postDatabaseMock"
 import { AuthenticatorMock } from "../mocks/services/AuthenticatorMock"
 import { HashManagerMock } from "../mocks/services/HashManagerMock"
@@ -8,5 +9,19 @@ describe("testando PostBusiness", () => {
     const postBusiness = new PostBusiness(new PostDatabaseMock(),
      new IdGeneratorMock(),
       new HashManagerMock(),
-      new AuthenticatorMock())
+      new AuthenticatorMock()
+      )
+
+      test("post criado com sucesso", async () => {
+        const input: ICreatePostInputDTO = {
+token: "token-astrodev", content: "olá!"
+        }
+
+        const response = await postBusiness.createPost(input)
+
+        expect(response.message).toEqual("post criado com sucesso")
+        expect(response.post.getId()).toEqual("id-mock")
+        expect(response.post.getContent()).toEqual("olá!")
+        expect(response.post.getUserId()).toEqual("101")
+      })
 })      
